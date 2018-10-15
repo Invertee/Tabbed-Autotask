a1d0f584d1a3618c86d2e31fc8988122f75579d2
Sam Petch <smpetch+github@gmail.com>
2018-10-15 23:13:42 +0100

    Added loop to prefill array

--- background.js
+++ background.js
@@ -11,21 +11,26 @@ function addPopup() {
 function removePopup() {
 	var setting = 'popups';
 	console.log(' Clearing setting for '+setting);
-	
 	chrome.contentSettings[setting].clear({});
 }
 // Add Autotask url to popup whitelist
 addPopup()
 
 // Gets last tab
-var preURL = ""
-chrome.webNavigation.onCommitted.addListener(function(object)
-	{ 
-		chrome.tabs.get(object.tabId, function(tab)
-		{
-			preURL = tab.url
-		});
-	});
+var preURL = []
+// Pushes url to array to make sure it works off the bat ;)
+for (var i = 0; i < 3; i++) {
+	preURL.push('https://ww4.autotask.net')
+}
+chrome.tabs.onActivated.addListener(function(w){
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) 
+	{
+		preURL.push( tabs[0].url )
+		if (preURL.length > 3) { preURL.shift() }
+		console.log(preURL)
+	});	
+});
+
 // Moves popup to end of window. 
 chrome.windows.getCurrent({},function(w){
 	var mainwindow = w.id;
@@ -36,10 +41,9 @@ chrome.windows.getCurrent({},function(w){
 					active: true,
 					windowId: w.id
 				}, function (tabs) {
-					console.log(preURL)
-					if (preURL.startsWith('https://ww4.autotask.net')){
+					if (preURL[1].startsWith('https://ww4.autotask.net')){
 						chrome.tabs.move(w.tabs[0].id,{windowId:mainwindow,index:-1},function(){
-							chrome.tabs.update(w.tabs[0].id,{active:true});
+						chrome.tabs.update(w.tabs[0].id,{active:true});
 						});
 					}
 				});
