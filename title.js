0f5bec2037f486e539ab49680a5ac5cebab6edd1
Invertee <smpetch+github@gmail.com>
2019-05-07 11:39:21 +0100

    Handling for AT tasks

--- title.js
+++ title.js
@@ -6,3 +6,12 @@ if (title.match(reg)) {
     var ntitle = title.match(regm)
     document.title = ntitle[1]
 };
+
+var title = document.title;
+var reg = /Task\W+T\d+\W\d+/gm;
+var regm = /Task\W+T\d+\W\d+...(.+)/;
+
+if (title.match(reg)) {
+    var ntitle = title.match(regm)
+    document.title = ntitle[1]
+};
